00ff8f6e50eb9a90877c7743a81ed1bb0a915f78
SSPENELOPE <typoepping@gmail.com>
2022-12-11 11:56:55 -0500

    added appointment page with link to customers calendly widget, changed some styling across the site

--- client/src/pages/Appointments.jsx
+++ client/src/pages/Appointments.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { InlineWidget } from "react-calendly";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 
 const Appointment = () => {
+
     return (
         <div>
             <Header />
-            <iframe src="https://app.squarespacescheduling.com/schedule.php?owner=27904854" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
+            <div>
+                {Auth.loggedIn() ? (
+                    <InlineWidget pageSettings={{
+                    backgroundColor: 'black',
+                    hideEventTypeDetails: false,
+                    hideLandingPageDetails: false,
+                    primaryColor: '00a2ff',
+                    textColor: 'white'
+                    }} url="https://calendly.com/pawsnclawspetcare" />
+
+                ) : (
+                    <div className="apptLogin">
+                        <h1 >Please <Link to="/login">Login</Link> to schedule an appointment</h1>
+                    </div>
+                )}
+
+            </div>
             <Footer />
         </div>
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,20 +23,25 @@ const Home = () => {
                     </div>
                     <div className="homesection text-center flex-wrap">
                         <h1 className="font"><u>WHO WE ARE</u></h1>
-                        <p className="flex-wrap">Welcome to Paws N Claws Pet Care where we love animals and make it our mission to ensure your's is taken care of while your away.</p>
+                        <p className="flex-wrap mission">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci dapibus ultrices in iaculis nunc sed. Et magnis dis parturient montes nascetur ridiculus mus mauris. Rhoncus aenean vel elit scelerisque mauris pellentesque. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Neque viverra justo nec ultrices. Etiam tempor orci eu lobortis elementum. Sit amet purus gravida quis. Id aliquet risus feugiat in ante metus dictum. Diam volutpat commodo sed egestas egestas fringilla phasellus. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna. Duis at tellus at urna condimentum mattis pellentesque id nibh. Luctus accumsan tortor posuere ac ut consequat. At tempor commodo ullamcorper a lacus vestibulum sed arcu. Posuere morbi leo urna molestie at.</p>
                     </div>
                 </div>
                 &nbsp;
                 <div className="d-flex flex-row">
-                    <div className="homesection text-center flex-wrap">
+                    <div className="homesection text-center flex-wrap services">
                         <h1 className="font"><u>OUR SERVICES</u></h1>
                         <p className="paragraph">At PAWS N CLAWS we specialize in animal care. While your away pick from a variety of services we offer to ensure your pet is taken care of while you're on that vaction you'd been wanting to take for months or maybe just out of town visting family. If you're not sure we offer the service always feel free to reach out and ask and we will do our best to ensure your animal is taken care of!</p>
+                        &nbsp;
+                        <div>
                         <h3>Here are some of the services we offer!</h3>
-                        <ul>
-                            <li>Dog Walking</li>
-                            <li>Overnight Stays with your pet</li>
-                            <li>Feeding and Bathroom Breaks</li>
-                        </ul>
+                            <ul style={{listStyle: "none"}}>
+                                <li>Dog Walking</li>
+                                <li>Overnight Stays with your pet</li>
+                                <li>Feeding and Bathroom Breaks</li>
+                            </ul>
+                        </div>
+                        &nbsp;
+                        <h6>Click here to get full list of our pricing and services!</h6>
                     </div>
                     <div className="homesection">
                         <img src={dogTrainer} />
