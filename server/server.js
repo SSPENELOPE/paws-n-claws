dad2787f672f4f01fe5765295bd24d7579cba893
SSPENELOPE <typoepping@gmail.com>
2022-12-18 20:27:52 -0500

    attempted heroku fix

--- server/server.js
+++ server/server.js
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 app.get('/', (req, res) => {
