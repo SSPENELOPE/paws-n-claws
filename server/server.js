a64fbf6e558bb4e771bb53ca92bbb388c41eb76b
SSPENELOPE <typoepping@gmail.com>
2022-12-18 20:24:28 -0500

    changed server.js to serve production build

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
 }
 
 app.get('/', (req, res) => {
